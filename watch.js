f9b2fbf81aa1f03a5da660c9a27c1dd2564b9cfd
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2023-03-23 04:28:07 +0100

    Improves the rendering of the table

--- watch.js
+++ watch.js
@@ -5,31 +5,39 @@ const {JSDOM} = jsdom;
 const dates = JSON.parse(await fs.promises.readFile("cache/dates.json"));
 const players = JSON.parse(await fs.promises.readFile("cache/players.json"));
 const {window} = new JSDOM(`\
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
 	<head>
 		<title>Watch</title>
 		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width" />
+		<meta name="color-scheme" content="dark light" />
 		<style>
 			* {
 				box-sizing: border-box;
 			}
+			html {
+				width: min-content;
+				height: min-content;
+			}
 			body {
+				margin: 20px;
 				font: 16px / 1.25 serif;
+				background: var(--canvas-background);
+				color: var(--canvas-foreground);
 			}
 			table {
 				table-layout: fixed;
 				border-collapse: collapse;
-				text-align: center;
-				vertical-align: middle;
+				background: var(--canvas-background);
 			}
 			col {
 				background: repeating-linear-gradient(45deg, var(--android, #0000) 0 2px, var(--ios, #0000) 0 4px);
 			}
 			col[data-android] {
-				--android: #ffc;
+				--android: var(--overlay-yellow);
 			}
 			col[data-ios] {
-				--ios: #ccf;
+				--ios: var(--overlay-blue);
 			}
 			thead {
 				writing-mode: sideways-lr;
@@ -38,39 +46,80 @@ const {window} = new JSDOM(`\
 			td {
 				min-width: 40px;
 				min-height: 40px;
-				padding: 0;
-				border: 1px solid #000;
+				text-align: center;
+				vertical-align: middle;
 			}
 			th {
 				padding: 10px;
 			}
-			th[data-android]::after {
+			th:not(:empty)[data-android]:not([data-ios])::after {
 				content: " (" attr(data-android) ")";
 			}
-			th[data-ios]::after {
+			th:not(:empty):not([data-android])[data-ios]::after {
 				content: " (" attr(data-ios) ")";
 			}
-			th[data-android][data-ios]::after {
+			th:not(:empty)[data-android][data-ios]::after {
 				content: " (" attr(data-android) ", " attr(data-ios) ")";
 			}
-			td:not(:first-of-type:empty, :last-of-type:empty) {
-				background: #3333;
+			td {
+				padding: 0;
+			}
+			th:not(:empty),
+			td:not(:empty) {
+				border: 1px solid var(--canvas-foreground);
+				background: var(--mark);
+			}
+			th:empty,
+			td:empty:is(:first-of-type, :last-of-type) {
+				border: 1px dashed var(--canvas-foreground);
+				background: var(--canvas-background);
+			}
+			td:empty:not(:first-of-type, :last-of-type) {
+				border-left: 1px dashed var(--canvas-foreground);
+				border-right: 1px dashed var(--canvas-foreground);
+				border-top: 1px solid var(--canvas-foreground);
+				border-bottom: 1px solid var(--canvas-foreground);
+				background: var(--mark);
 			}
 			a {
 				display: block;
 				padding: 10px;
-				background: #6f6;
-				color: #000;
+				background: var(--known);
+				color: inherit;
 			}
 			a[data-platform="ios"] {
-				outline: 5px solid #66f;
+				outline: 5px solid var(--highlight);
 				outline-offset: -5px;
 			}
 			a:empty {
-				background: #f66
+				background: var(--unknown);
 			}
 			a:empty::before {
 				content: "?";
+			}
+			@media (prefers-color-scheme: dark) {
+				:root {
+					--canvas-background: #000;
+					--canvas-foreground: #ccc;
+					--overlay-yellow: #630;
+					--overlay-blue: #036;
+					--mark: #6663;
+					--highlight: #999c;
+					--known: #363;
+					--unknown: #636;
+				}
+			}
+			@media (prefers-color-scheme: light) {
+				:root {
+					--canvas-background: #fff;
+					--canvas-foreground: #333;
+					--overlay-yellow: #fc9;
+					--overlay-blue: #9cf;
+					--mark: #9993;
+					--highlight: #666c;
+					--known: #9c9;
+					--unknown: #c9c;
+				}
 			}
 		</style>
 	</head>
@@ -82,12 +131,14 @@ const {document} = window;
 const html = document.documentElement;
 const body = document.createElement("body");
 const table = document.createElement("table");
-const colgroup = document.createElement("colgroup");
-const col = document.createElement("col");
+const colhead = document.createElement("colgroup");
+const colbody = document.createElement("colgroup");
 const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
+const col = document.createElement("col");
 const tr = document.createElement("tr");
 const th = document.createElement("th");
-colgroup.append(col);
+colhead.append(col);
 tr.append(th);
 for (const [date, datePlatforms] of Object.entries(dates)) {
 	const col = document.createElement("col");
@@ -97,35 +148,36 @@ for (const [date, datePlatforms] of Object.entries(dates)) {
 		col.setAttribute(`data-${platform}`, datePlatforms[platform]);
 		th.setAttribute(`data-${platform}`, datePlatforms[platform]);
 	}
-	colgroup.append(col);
+	colbody.append(col);
 	tr.append(th);
 }
 thead.append(tr);
-table.append(colgroup);
-table.append(thead);
-const tbody = document.createElement("tbody");
 for (const [player, playerDates] of Object.entries(players)) {
 	const tr = document.createElement("tr");
 	const th = document.createElement("th");
 	th.textContent = player;
 	tr.append(th);
 	let span = 0;
+	const arrival = Object.keys(playerDates)[0];
+	const departure = Object.keys(playerDates)[Object.keys(playerDates).length - 1];
 	for (const date of Object.keys(dates)) {
-		if (playerDates[date] != null) {
+		if (date >= arrival && date <= departure && (playerDates[date] != null || Object.keys(dates[date]).length !== 0)) {
 			if (span > 0) {
 				const td = document.createElement("td");
 				td.colSpan = span;
 				tr.append(td);
 			}
 			const td = document.createElement("td");
-			for (const run of playerDates[date]) {
-				const p = document.createElement("p");
-				const a = document.createElement("a");
-				a.href = run.href;
-				a.textContent = run.textContent;
-				a.setAttribute("data-platform", run.platform);
-				p.append(a);
-				td.append(p);
+			if (playerDates[date] != null) {
+				for (const run of playerDates[date]) {
+					const p = document.createElement("p");
+					const a = document.createElement("a");
+					a.href = run.href;
+					a.textContent = run.textContent;
+					a.setAttribute("data-platform", run.platform);
+					p.append(a);
+					td.append(p);
+				}
 			}
 			tr.append(td);
 			span = 0;
@@ -140,6 +192,9 @@ for (const [player, playerDates] of Object.entries(players)) {
 	}
 	tbody.append(tr);
 }
+table.append(colhead);
+table.append(colbody);
+table.append(thead);
 table.append(tbody);
 body.append(table);
 html.append(body);
